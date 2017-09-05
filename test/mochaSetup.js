d6d372a40a3306882dd11a312a424da8646453a0
David Beaumont <david.beaumont@holidayextras.com>
2017-09-05 13:34:25 +0100

    Update dependencies

--- test/mochaSetup.js
+++ test/mochaSetup.js
@@ -1,10 +1,14 @@
 'use strict'
 
-const jsdom = require('jsdom')
+const jsdom = require('jsdom/lib/old-api')
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
 
 global.XMLHttpRequest = XMLHttpRequest
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
-global.window = document.defaultView
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+window.locale = 'en'
+
+// Workaround for https://github.com/chaijs/type-detect/pull/91
+global.HTMLElement = window.HTMLElement
 
 window.XMLHttpRequest = XMLHttpRequest
